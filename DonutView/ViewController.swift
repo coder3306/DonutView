8e6794c5a7925b2eb1e6a5e4ec4b872da0f431d0
coder3306 <coder3306@goodsoft.io>
2023-07-26 10:48:45 +0900

    Initial Commit

---
+++ DonutView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DonutView
+//
+//  Created by 정성규 on 2023/07/26
+//  Copyright © 2023 ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ DonutViewUITests/DonutViewUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  DonutViewUITestsLaunchTests.swift
+//  DonutViewUITests
+//
+//  Created by 정성규 on 2023/07/26
+//  Copyright © 2023 ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+import XCTest
+
+final class DonutViewUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
